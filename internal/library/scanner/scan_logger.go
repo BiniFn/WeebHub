4e695225311e0b8971755bc0ac5610fb91e1419f
BiniFn <albingjana.official@gmail..com>
2026-05-17 10:17:35 +0200

    Fix bugs in backend

--- internal/library/scanner/scan_logger.go
+++ internal/library/scanner/scan_logger.go
@@ -16,7 +16,7 @@ type ScanLogger struct {
 	logger  *zerolog.Logger
 	logFile *os.File
 	buffer  *bytes.Buffer
-	mu      sync.Mutex
+	mu      *sync.Mutex
 }
 
 // NewScanLogger creates a new ScanLogger with a log file named based on the current datetime.
@@ -42,10 +42,10 @@ func NewScanLogger(outputDir string) (*ScanLogger, error) {
 	// Create a buffer for storing log entries
 	buffer := new(bytes.Buffer)
 
-	mu := sync.Mutex{}
+	mu := &sync.Mutex{}
 
 	// Create an array writer to wrap the JSON encoder
-	logger := zerolog.New(ThreadSafeWriteSyncer{buffer, &mu}).With().Logger()
+	logger := zerolog.New(ThreadSafeWriteSyncer{buffer, mu}).With().Logger()
 
 	return &ScanLogger{&logger, logFile, buffer, mu}, nil
 }
@@ -60,7 +60,7 @@ func NewConsoleScanLogger() (*ScanLogger, error) {
 	// Create an array writer to wrap the JSON encoder
 	logger := zerolog.New(output).With().Logger()
 
-	return &ScanLogger{logger: &logger, logFile: nil, buffer: nil, mu: sync.Mutex{}}, nil
+	return &ScanLogger{logger: &logger, logFile: nil, buffer: nil, mu: &sync.Mutex{}}, nil
 }
 
 func (sl *ScanLogger) LogMediaContainer(level zerolog.Level) *zerolog.Event {
