992d6f276c7530e8608b1ab059f7d49b15a67d60
BiniFn <albingjana.official@gmail..com>
2026-05-17 10:05:02 +0200

    Fix video player compositing on Android

--- weebhub-web/capacitor.config.ts
+++ weebhub-web/capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.weebhub.app',
   appName: 'WeebHub',
-  webDir: 'out'
+  webDir: 'out',
+  backgroundColor: '#00000000'
 };
 
 export default config;
