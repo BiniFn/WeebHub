4c811ff7b899b8054e9ae7bcd1280d7cb278da4c
BiniFn <albingjana.official@gmail..com>
2026-05-17 14:51:38 +0200

    Auto enable streamer mode in OBS and release 3.8.5

--- weebhub-web/src/app/(main)/_atoms/streamer-mode.atoms.ts
+++ weebhub-web/src/app/(main)/_atoms/streamer-mode.atoms.ts
@@ -1,12 +1,21 @@
 import { atomWithStorage } from "jotai/utils"
 import React from "react"
-import { useAtomValue } from "jotai"
+import { useAtom } from "jotai"
 
 export const streamerModeAtom = atomWithStorage<boolean>("sea-streamer-mode", false)
 
 export function useStreamerModeEffect() {
-    const isStreamerMode = useAtomValue(streamerModeAtom)
+    const [isStreamerMode, setIsStreamerMode] = useAtom(streamerModeAtom)
     
+    React.useEffect(() => {
+        if (typeof window !== "undefined") {
+            // Automatically enable streamer mode when loaded in OBS Browser Source
+            if (window.navigator.userAgent.includes("OBS") || window.navigator.userAgent.includes("OBSBrowser")) {
+                setIsStreamerMode(true)
+            }
+        }
+    }, [setIsStreamerMode])
+
     React.useEffect(() => {
         if (typeof document === "undefined") return
         if (isStreamerMode) {
